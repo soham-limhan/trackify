7955a7865c0442b9d45762fc97b63dde4b03a354
soham-limhan <sohamlimhan002@gmail.com>
2026-03-30 10:20:37 +0530

    Bug Fixed: Login

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 import auth
